e716322981e1c34298296d4155129e86182c2fd3
Taniraw <tanirawrites@gmail.com>
2016-09-15 10:18:20 -0400

    UI Card Lab Completed

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -13,7 +13,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //♣️ ♠️ ♦️♥️
+    }
+    
+   
+    @IBOutlet weak var displaySuit: UILabel!
+    @IBOutlet weak var displaySuit2: UILabel!
+    
+    @IBOutlet weak var middleValue: UILabel!
+    
+   
+    
+    
+    @IBAction func club(sender: AnyObject) {
+        displaySuit.text = "♣️"
+        displaySuit2.text = "♣️"
+        middleValue.text = "4"
+        
+    }
+    @IBAction func spade(sender: AnyObject) {
+        displaySuit.text = "♠️"
+        displaySuit2.text = "♠️"
+        middleValue.text = "3"
+    }
+    @IBAction func diamond(sender: AnyObject) {
+        displaySuit.text = "♦️"
+        displaySuit2.text = "♦️"
+        middleValue.text = "8"
+        
+    }
+    @IBAction func heart(sender: AnyObject) {
+        displaySuit.text = "♥️"
+        displaySuit2.text = "♥️"
+        middleValue.text = "10"
+    }
+   
+   
+        
     }
 
     // TODO: IB actions and code to update UI
-}
+
